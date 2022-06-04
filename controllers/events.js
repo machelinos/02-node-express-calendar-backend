7b7fec779c8b238f35adb5ca1932d1ec86827d20
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-06-04 01:18:52 -0500

    Create event to db

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,6 @@
 const { request, response } = require('express');
+const Event = require('../models/Event');
+
 
 const getEvents = (req = request, res = response) => {
     return res.status(200).json({
@@ -7,13 +9,29 @@ const getEvents = (req = request, res = response) => {
     });
 }
 
-const createEvent = (req = request, res = response) => {
+const createEvent = async (req = request, res = response) => {
     console.log(req.body);
+
+    //Save to database
+    try {
+        const eventModel = new Event(req.body);
+        eventModel.user = req.uid;
+
+        const dbEvent = await eventModel.save();
+
+        return res.status(201).json({
+            ok: true,
+            event: dbEvent
+        });
     
-    return res.status(201).json({
-        ok: true,
-        msg: 'Create event'
-    });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'There was an error creating the event. please contact admin'
+        });
+    }    
 }
 
 const updateEvent = (req = request, res = response) => {
