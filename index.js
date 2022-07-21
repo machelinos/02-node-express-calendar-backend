526798e20d1edf7e5250285b4c62ecf7f17959bd
machelinos <marcel.cabrera@gmail.com>
2022-07-20 23:44:52 -0500

    Added * to express routes

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.use(express.static('public'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+})
+
 // Listener
 app.listen(process.env.PORT, ()=>{
     console.log(`Servidor corriendo en puerto:${process.env.PORT}` );
